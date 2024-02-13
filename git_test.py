7a939e9ab5707ddf68d1c31b44e5f86ee5deb7b6
Elena <elena.silvestricecinelli@gmail.com>
2024-02-13 20:49:49 +0000

    Wrote the code for the investment calculator

--- git_test.py
+++ git_test.py
@@ -17,3 +17,18 @@ user_choice = input("Enter either 'investment' or 'bond' from the menu above to 
 
 simple_interest = "simple"
 compound_interest = "compound"
+
+# Investment calculator 
+
+if user_choice == investment:
+    deposit = int(input("How much money do you want to deposit? ")) 
+    interest_percentage = int(input("Please tell us the percentage of the interest rate (no symbols): "))/100
+    years = int(input("How many years do you plan on investing? "))
+    interest = input("Do you want a simple or compound interest? ").lower()
+
+    if interest == simple_interest:
+        print(deposit *(1+interest_percentage*years))
+    elif interest == compound_interest:
+        print(deposit * math.pow((1+interest_percentage),years))
+    else:
+        print("Please start again and choose either 'simple' or 'compound.")
